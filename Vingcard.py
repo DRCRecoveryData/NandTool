1c9d7c41ae40c0dea370a5bfd4a095b7d21d7ab3
DRC Lab <85211068+DRCRecoveryData@users.noreply.github.com>
2024-04-27 19:29:30 +0700

    Update Vingcard.py
    Update to python 3

--- Vingcard.py
+++ Vingcard.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 #############################################################################
@@ -15,12 +15,12 @@ from scapy.all import *
 
 
 class MifareSerial(Packet):
-    fields_desc=[
+    fields_desc = [
         ByteEnumField("manufacturer", 4, {0x04: "NXP"}),
-        XShortField("serial1", 0),
-        XByteField("check0", 0),
-        XIntField("serial2", 0),
-        XByteField("check1", 0)
+        ShortField("serial1", 0),
+        ByteField("check0", 0),
+        IntField("serial2", 0),
+        ByteField("check1", 0)
     ]
 
     def i2repr(self, pkt):
@@ -28,47 +28,47 @@ class MifareSerial(Packet):
         
 
 class MifareUltralight(Packet):
-    fields_desc=[
-        MifareSerial,
+    fields_desc = [
+        PacketField("mifare_serial", None, MifareSerial),
         ByteField("internal", 0),
-        XShortField("lockbytes", 0),
-        XIntField("otp_bytes", 0)
+        ShortField("lockbytes", 0),
+        IntField("otp_bytes", 0)
     ]
 
 
 class VingcardProtected(Packet):
-    fields_desc=[
-        XByteField("xor_key", 0),
-        XByteField("magic", 0x90),
-        X3BytesField("hotel_id", 0),
-        XByteField("key_type", 0x82),
-        XIntField("key_id", 0),
-        XIntField("duration", 0),
-        X3BytesField("room_id", 0),
-        XByteField("unk1", 0),
-        XShortField("const1", 0xFF00),
-        XIntField("unk2", 0)
+    fields_desc = [
+        ByteField("xor_key", 0),
+        ByteField("magic", 0x90),
+        ThreeBytesField("hotel_id", 0),
+        ByteField("key_type", 0x82),
+        IntField("key_id", 0),
+        IntField("duration", 0),
+        ThreeBytesField("room_id", 0),
+        ByteField("unk1", 0),
+        ShortField("const1", 0xFF00),
+        IntField("unk2", 0)
     ]
 
     def pre_dissect(self, s):
-        k = ord(s[0])
-        return s[0] + "".join([chr(k ^ ord(x)) for x in s[1:]])
+        k = s[0]
+        return bytes([k]) + bytes([k ^ x for x in s[1:]])
 
     def post_build(self, p, pay):
-        k = ord(p[0])
-        return p[0] + "".join([chr(k ^ ord(x)) for x in p[1:]]) + "".join([chr(k ^ ord(x)) for x in pay])
+        k = p[0]
+        return bytes([k]) + bytes([k ^ x for x in p[1:]]) + bytes([k ^ x for x in pay])
 
 
 class Vingcard(Packet):
-    fields_desc=[
-        MifareUltralight,
+    fields_desc = [
+        PacketField("mifare_ultralight", None, MifareUltralight),
         ByteField("magic", 6),
         FieldLenField("header_length", None, length_of="unknown", fmt="B"),
         FieldLenField("payload_length", None, length_of="protected_data", fmt="H"),
         StrLenField("unknown", "", length_from=lambda pkt: pkt.header_length - 4),
         PacketLenField("protected_data", None, VingcardProtected, length_from=lambda pkt: pkt.payload_length),
         ByteField("unk", 0),
-        XIntField("checksum", None)
+        IntField("checksum", None)
     ]
 
 
@@ -76,16 +76,16 @@ if __name__ == "__main__":
     import argparse
     import sys
 
-    parser = argparse.ArgumentParser(sys.argv[0])
+    parser = argparse.ArgumentParser()
     parser.add_argument('dump', metavar="FILE", nargs=1, type=argparse.FileType('rb'))
-    args = parser.parse_args(sys.argv[1:])
+    args = parser.parse_args()
     try:
-        Vingcard(args.dump[0].read()).show2()
+        pkt = Ether(args.dump[0].read())
+        pkt.show2()
     except:
-        print >> sys.stderr, "Input file does not seem to be a valid Vingcard dump"
+        print("Input file does not seem to be a valid Vingcard dump", file=sys.stderr)
     finally:
         args.dump[0].close()
     sys.exit(0)
 
 # vim:ts=4:expandtab:sw=4
-
